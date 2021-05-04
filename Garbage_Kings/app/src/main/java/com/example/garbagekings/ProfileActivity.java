35be94760bf2e27e265bb63eb63cee903d3e887c
nikit <derfunov.ns@edu.spbstu.ru>
2021-05-04 04:58:39 +0300

    Add settings and do button on profile

--- Garbage_Kings/app/src/main/java/com/example/garbagekings/ProfileActivity.java
+++ Garbage_Kings/app/src/main/java/com/example/garbagekings/ProfileActivity.java
@@ -1,8 +1,12 @@
 package com.example.garbagekings;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,10 +17,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 
 public class ProfileActivity extends Fragment {
 
+    Button btnSettings;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_profile, container, false);
+        btnSettings = (Button) v.findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
         return v;
     }
+
 }
