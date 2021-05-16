acdc6eeea783d0bc6896308a87b6a845ac68deac
1yarix <jarik366@gmail.com>
2021-05-16 22:55:50 +0300

    added profile activity functional

--- Garbage_Kings/app/src/main/java/com/example/garbagekings/ProfileActivity.java
+++ Garbage_Kings/app/src/main/java/com/example/garbagekings/ProfileActivity.java
@@ -1,28 +1,36 @@
 package com.example.garbagekings;
 
-import androidx.annotation.IdRes;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
-import com.google.android.gms.maps.SupportMapFragment;
+import com.example.garbagekings.Modules.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ProfileActivity extends Fragment {
 
+    FirebaseFirestore db_fs = FirebaseFirestore.getInstance();
+    TextView mUserName;
+    TextView mEmail;
+    TextView mPhoneNumber;
     Button btnSettings;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_profile, container, false);
+
         btnSettings = (Button) v.findViewById(R.id.btnSettings);
         btnSettings.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +39,35 @@ public class ProfileActivity extends Fragment {
                 startActivity(intent);
             }
         });
+
+        mUserName = v.findViewById(R.id.tvUserName);
+        mEmail = v.findViewById(R.id.tvEmail);
+        mPhoneNumber = v.findViewById(R.id.tvPhoneNumber);
+
+        String id = FirebaseAuth.getInstance().getUid();
+        if (id == null) {
+            mUserName.setText("Неавторизованный пользователь");
+            mEmail.setText("Неавторизованный пользователь");
+            mPhoneNumber.setText("Неавторизованный пользователь");
+        }
+        else {
+            db_fs.collection("users").document(id)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                User currentUser = document.toObject(User.class);
+
+                                mUserName.setText(currentUser.getName());
+                                mEmail.setText(currentUser.getEmail());
+                                mPhoneNumber.setText(currentUser.getPhone());
+                            }
+                        }
+                    });
+        }
+
         return v;
     }
 
